bd01a814a7bc22a0d5e97438097f538a9de0fe79
Surtsev <surtsev2006@gmail.com>
2026-04-03 19:30:11 +0300

    add pydantic and fix mark types in models

--- src/infra/models.py
+++ src/infra/models.py
@@ -1,13 +1,11 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, Enum as SQLEnum
-from sqlalchemy.orm import relationship, declarative_base
-import enum
+from sqlalchemy import Column
+from sqlalchemy import Enum as SQLEnum
+from sqlalchemy import Float, ForeignKey, Integer, String
+from sqlalchemy.orm import declarative_base, relationship
+
+from src.config.types import MarkTypes
 
 Base = declarative_base()
-
-
-class MarkTypes(enum.Enum):
-    MARK = 0
-    GOAL = 1
 
 
 class Mark(Base):
@@ -18,11 +16,7 @@ class Mark(Base):
     current = Column(Float, nullable=False, default=0)
     required = Column(Integer, nullable=False, default=0)
 
-    cards = relationship(
-        "Card",
-        back_populates="mark",
-        cascade="all, delete-orphan"
-    )
+    cards = relationship("Card", back_populates="mark", cascade="all, delete-orphan")
 
 
 class Card(Base):
@@ -32,9 +26,7 @@ class Card(Base):
     name = Column(String, nullable=False)
     value = Column(Float, nullable=False, default=0)
     mark_name = Column(
-        String,
-        ForeignKey("marks.name", ondelete="CASCADE"),
-        nullable=False
+        String, ForeignKey("marks.name", ondelete="CASCADE"), nullable=False
     )
 
     mark = relationship("Mark", back_populates="cards")
